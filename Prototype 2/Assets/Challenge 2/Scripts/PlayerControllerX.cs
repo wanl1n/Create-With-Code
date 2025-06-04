e62b2c9de04c391382253e1dc22880b916fa569b
wanl1n <kate_young@dlsu.edu.ph>
2025-06-04 13:02:26 +0800

    Prototype 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 2;
+    private float timer = 0;
+    private bool spawned = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > cooldown && spawned)
+        {
+            timer = 0;
+            spawned = false;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawned = true;
+            timer = 0;
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+            Destroy(gameObject);
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}

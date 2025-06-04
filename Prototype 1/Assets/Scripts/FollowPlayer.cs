83604f36b0e5d20b03cb0b6868cf4cb8449703f8
wanl1n <kate_young@dlsu.edu.ph>
2025-06-04 11:26:46 +0800

    create with code init

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}

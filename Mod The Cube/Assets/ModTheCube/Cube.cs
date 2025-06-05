32a598793a8bbc13f29bda8d325a104a53604b05
wanl1n <kate_young@dlsu.edu.ph>
2025-06-05 11:27:14 +0800

    prototype 3

---
+++ Mod The Cube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private int n = 5.0f;
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        float randomPosX = Random.Range(-10, 10); 
+        float randomPosY = Random.Range(-10, 10); 
+        float randomPosZ = Random.Range(-10, 10);
+        transform.position = new Vector3(randomPosX, randomPosY, randomPosZ);
+
+        transform.Rotate(20.0f * Time.deltaTime, 10.0f * Time.deltaTime, 0.0f);
+    }
+}
---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    private PlayerController playerController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        if (!playerController.gameOver)
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
